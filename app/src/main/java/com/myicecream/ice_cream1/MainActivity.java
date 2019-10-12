7e6d86aa9cf3b7b2202d772a5ebeb68f9514c7c4
Omoyi <urakhel7@gmail.com>
2019-10-12 12:37:41 +0200

    connect my home button to another activity

--- app/src/main/java/com/myicecream/ice_cream1/MainActivity.java
+++ app/src/main/java/com/myicecream/ice_cream1/MainActivity.java
@@ -2,15 +2,28 @@ package com.myicecream.ice_cream1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        homeButton = (Button) findViewById(R.id.homeButton);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gallery = new Intent(MainActivity.this, icecreamGallery.class);
+                startActivity(gallery);
+            }
 
+        });
 
     }
 }
--- app/src/main/java/com/myicecream/ice_cream1/icecreamGallery.java
+++ app/src/main/java/com/myicecream/ice_cream1/icecreamGallery.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class icecreamGallery extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icecream_gallery);
+
+
     }
 }
