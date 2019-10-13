daea41e3a2ac13b7237d1d2c67ffbe33940f9480
Omoyi <urakhel7@gmail.com>
2019-10-13 20:37:24 +0200

    create a constructor in the adapter class and style my list view

--- app/src/main/java/com/myicecream/ice_cream1/iceCreamList.java
+++ app/src/main/java/com/myicecream/ice_cream1/iceCreamList.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -12,12 +15,18 @@ import butterknife.ButterKnife;
 public class iceCreamList extends AppCompatActivity {
 
     @BindView(R.id.textView) TextView title;
-    @BindView(R.id.iceList) TextView list;
+    @BindView(R.id.iceList) ListView list;
 
     private String[] collection = new String[] {
-            "Vanilla Ice Cream.", "Cookies Ice Cream.", "Cream Ice Cream.", "", ""
+            "Vanilla Ice Cream.", "Cookies Ice Cream.", "Cream Ice Cream.", "Birthday Cake Ice Cream.", "Bubble Gum Ice Cream.", "Strawberry Ice Cream.",
+            "Black Cherry Ice Cream.", "Sundae Ice Cream", "Chocolate Peanut Butter Ice Cream.", "Cookie Dough"
+            , "Mint Chocolate Chip."
     };
-
+    private String[] place = new String[] {
+            "Sweet Hereafter", "Cricket", "Red Square", "Kigali heights", "Slappy Cakes", "Chez lando",
+            "Fast food hub", "Horse Brass", "la Palis", "Bambino"
+            , "Digest ltd"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,8 +37,8 @@ public class iceCreamList extends AppCompatActivity {
         String getInput = ices.getStringExtra("Rachel's Ice cream");
         title.setText(getInput);
 
-//        icecreamAdapter listMaker = new icecreamAdapter(this, android.R.layout.simple_selectable_list_item, collection );
-//        list.setAdapter(listMaker);
+        icecreamAdapter listMaker = new icecreamAdapter(this, android.R.layout.simple_selectable_list_item, collection, place );
+        list.setAdapter(listMaker);
 
 
     }
--- app/src/main/java/com/myicecream/ice_cream1/icecreamAdapter.java
+++ app/src/main/java/com/myicecream/ice_cream1/icecreamAdapter.java
@@ -3,11 +3,29 @@ package com.myicecream.ice_cream1;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
-import androidx.annotation.NonNull;
-
 public class icecreamAdapter extends ArrayAdapter {
     private Context type;
     private String[] collection;
-    private String[] col2;
+    private String[] place;
+
+    public icecreamAdapter(Context type,  int resource, String[] collection, String[] place) {
+        super(type, resource);
+        this.type = type;
+        this.collection = collection;
+        this.place = place;
+
+    }
+
+    @Override
+    public Object getItem(int position) {
+        String restaurant = collection[position];
+        String cuisine = place[position];
+        return String.format("%s \nServes great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount() {
+        return collection.length;
+    }
 
 }
