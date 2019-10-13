fc7433988278a9ba6730ee3b08f84cc4fd1d8610
Omoyi <urakhel7@gmail.com>
2019-10-13 16:09:04 +0200

    connect the go button to an activity using an intent

--- app/src/main/java/com/myicecream/ice_cream1/icecreamGallery.java
+++ app/src/main/java/com/myicecream/ice_cream1/icecreamGallery.java
@@ -4,21 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 
 public class icecreamGallery extends AppCompatActivity {
 
     private EditText myInput;
+    private Button galButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icecream_gallery);
-        String icecreams = myInput.getText().toString();
 
-        Intent ices = new Intent(icecreamGallery.this, iceCreamList.class);
-        startActivity(ices);
+        galButton = (Button) findViewById(R.id.galButton);
+        galButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ices = new Intent(icecreamGallery.this, iceCreamList.class);
+                startActivity(ices);
+            }
+        });
 
     }
 }
