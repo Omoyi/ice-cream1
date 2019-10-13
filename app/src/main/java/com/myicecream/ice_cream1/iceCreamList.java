2921a7ec78e7f65083fc1253e5e6016ec7d909d9
Omoyi <urakhel7@gmail.com>
2019-10-13 17:31:07 +0200

    change font

--- app/src/main/java/com/myicecream/ice_cream1/iceCreamList.java
+++ app/src/main/java/com/myicecream/ice_cream1/iceCreamList.java
@@ -11,6 +11,6 @@ public class iceCreamList extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.list_icecreams);
+        setContentView(R.layout.list_icecream);
     }
 }
