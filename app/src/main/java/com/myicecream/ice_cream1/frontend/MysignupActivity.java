7ed423c20ae72820f8e6fb139515bd2bab1f011c
Omoyi <urakhel7@gmail.com>
2019-10-25 16:44:00 +0200

    create a signup activity

---
+++ app/src/main/java/com/myicecream/ice_cream1/frontend/MysignupActivity.java
@@ -0,0 +1,17 @@
+
+package com.myicecream.ice_cream1.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.myicecream.ice_cream1.R;
+
+public class MysignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mysignup);
+    }
+}
