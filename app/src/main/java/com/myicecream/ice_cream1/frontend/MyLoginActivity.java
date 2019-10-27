9cef13bf1b7764a0e23f08e91ce13cdd531bc412
Omoyi <urakhel7@gmail.com>
2019-10-27 20:08:46 +0200

    recreate the sign up

--- app/src/main/java/com/myicecream/ice_cream1/frontend/MyLoginActivity.java
+++ app/src/main/java/com/myicecream/ice_cream1/frontend/MyLoginActivity.java
@@ -1,14 +1,32 @@
 package com.myicecream.ice_cream1.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import com.myicecream.ice_cream1.R;
 
-public class MyLoginActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class MyLoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.goToSignUpId) TextView toSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_login);
+    }
+
+    @Override
+    public void onClick(View account) {
+       if (account == toSignUp) {
+           Intent accounts = new Intent(MyLoginActivity.this, MysignupActivity.class);
+           startActivity(accounts);
+           finish();
+       }
     }
 }
--- app/src/main/java/com/myicecream/ice_cream1/frontend/MysignupActivity.java
+++ app/src/main/java/com/myicecream/ice_cream1/frontend/MysignupActivity.java
@@ -2,16 +2,20 @@
 package com.myicecream.ice_cream1.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
 import com.myicecream.ice_cream1.R;
 
-public class MysignupActivity extends AppCompatActivity {
+public class MysignupActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mysignup);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
