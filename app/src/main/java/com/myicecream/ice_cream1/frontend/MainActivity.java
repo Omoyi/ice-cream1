027488057f54b5d9c666441fe33c1a61b590fa5a
Omoyi <urakhel7@gmail.com>
2019-10-28 08:41:49 +0200

    insert in a log out menu

--- app/src/main/java/com/myicecream/ice_cream1/frontend/MainActivity.java
+++ app/src/main/java/com/myicecream/ice_cream1/frontend/MainActivity.java
@@ -3,8 +3,13 @@ package com.myicecream.ice_cream1.frontend;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 import com.myicecream.ice_cream1.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,5 +29,26 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
     }
 }
