4b85d2b701d37d5ecdb37d8e19e02a7824841a39
Omoyi <urakhel7@gmail.com>
2019-11-03 18:04:00 +0200

    create an activity and a layout file

---
+++ app/src/main/java/com/myicecream/ice_cream1/frontend/AddIcecreamActivity.java
@@ -0,0 +1,14 @@
+package com.myicecream.ice_cream1.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddIcecreamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_icecream);
+    }
+}
